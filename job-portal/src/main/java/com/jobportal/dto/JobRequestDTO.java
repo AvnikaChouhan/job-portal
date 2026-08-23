4037c8f7ee5550234d955ae14a7425e6036df561
Avnika Chouhan <avnikachouhan1@gmail.com>
2026-08-23 11:42:27 +0530

    feat: add job request DTO

---
+++ job-portal/src/main/java/com/jobportal/dto/JobRequestDTO.java
@@ -0,0 +1,54 @@
+package com.jobportal.dto;
+
+public class JobRequestDTO {
+
+        private String title;
+        private String description;
+        private String location;
+        private String company;
+        private double salary;
+
+        public JobRequestDTO() {
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
+
